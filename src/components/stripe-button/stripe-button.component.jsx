624332c8f3f3f5d558633ec9e3ad303f5c218aab
mgupta28 <mgupta28@in.ibm.com>
2020-10-31 12:17:29 +0530

    update router

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HiDQFDf0n4d6ADPl9v6kZMGhlzLWwG0K1YdATWGMqnfytqd8zWwuJRoRQ6lL5AklW7En73VSt2vE3FDe37ieVYS001UbqlxYz';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
